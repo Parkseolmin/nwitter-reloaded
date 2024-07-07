0c48efefdf2ca2996940d83278a6666aae18f4aa
Parkseolmin <qkrtjfals2@naver.com>
2024-07-07 17:36:57 +0900

    routes setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,52 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './routes/Home';
+import Profile from './routes/Profile';
+import Login from './routes/Login';
+import CreateAccount from './routes/CreateAccount';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+* {
+box-sizing: border-box;
+}
+body {
+background-color:black;
+color:white;
+}
+`;
 export default function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
 }
